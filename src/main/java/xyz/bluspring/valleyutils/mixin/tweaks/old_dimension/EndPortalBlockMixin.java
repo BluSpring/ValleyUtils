a0c9882b348e242790e0e3a038d9db8b1a714778
Naz <bluspringindohq@gmail.com>
2025-04-01 19:07:29 +0800

    AAAAAAAAAAAAAA

--- src/main/java/xyz/bluspring/valleyutils/mixin/tweaks/old_dimension/EndPortalBlockMixin.java
+++ src/main/java/xyz/bluspring/valleyutils/mixin/tweaks/old_dimension/EndPortalBlockMixin.java
@@ -1,9 +1,6 @@
 package xyz.bluspring.valleyutils.mixin.tweaks.old_dimension;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EndPortalBlock;
@@ -11,9 +8,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.bluspring.valleyutils.ValleyUtils;
 
 @Mixin(EndPortalBlock.class)
 public class EndPortalBlockMixin {
@@ -22,15 +17,5 @@ public class EndPortalBlockMixin {
         if (level.dimension().equals(Level.OVERWORLD)) {
             ci.cancel();
         }
-    }
-
-    @ModifyVariable(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getLevel(Lnet/minecraft/resources/ResourceKey;)Lnet/minecraft/server/level/ServerLevel;"))
-    private ResourceKey<Level> checkOldDimension(ResourceKey<Level> value, @Local(argsOnly = true) Level level) {
-        if (level.dimension().location().equals(ValleyUtils.id("old_overworld")))
-            return ResourceKey.create(Registries.DIMENSION, ValleyUtils.id("old_end"));
-        else if (level.dimension().location().equals(ValleyUtils.id("old_end")))
-            return ResourceKey.create(Registries.DIMENSION, ValleyUtils.id("old_overworld"));
-
-        return value;
     }
 }
--- src/main/java/xyz/bluspring/valleyutils/mixin/tweaks/old_dimension/EntityMixin.java
+++ src/main/java/xyz/bluspring/valleyutils/mixin/tweaks/old_dimension/EntityMixin.java
@@ -1,50 +1,8 @@
 package xyz.bluspring.valleyutils.mixin.tweaks.old_dimension;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import xyz.bluspring.valleyutils.ValleyUtils;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-    @Shadow public abstract Level level();
-
-    @ModifyExpressionValue(method = "findDimensionEntryPoint", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;dimension()Lnet/minecraft/resources/ResourceKey;"))
-    private ResourceKey<Level> addOldDimensionHandling(ResourceKey<Level> original) {
-        if (this.level().dimension() == ValleyUtils.OLD_END)
-            return Level.END;
-        else if (this.level().dimension() == ValleyUtils.OLD_NETHER)
-            return Level.NETHER;
-        else if (this.level().dimension() == ValleyUtils.OLD_OVERWORLD)
-            return Level.OVERWORLD;
-
-        return original;
-    }
-
-    @ModifyExpressionValue(method = "findDimensionEntryPoint", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;dimension()Lnet/minecraft/resources/ResourceKey;"))
-    private ResourceKey<Level> addOldDimensionHandling2(ResourceKey<Level> original) {
-        if (this.level().dimension() == ValleyUtils.OLD_END)
-            return Level.END;
-        else if (this.level().dimension() == ValleyUtils.OLD_NETHER)
-            return Level.NETHER;
-        else if (this.level().dimension() == ValleyUtils.OLD_OVERWORLD)
-            return Level.OVERWORLD;
-
-        return original;
-    }
-
-    @ModifyArg(method = "handleNetherPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getLevel(Lnet/minecraft/resources/ResourceKey;)Lnet/minecraft/server/level/ServerLevel;"))
-    private ResourceKey<Level> useOldDimension(ResourceKey<Level> dimension) {
-        if (this.level().dimension() == ValleyUtils.OLD_NETHER)
-            return ValleyUtils.OLD_OVERWORLD;
-        else if (this.level().dimension() == ValleyUtils.OLD_OVERWORLD)
-            return ValleyUtils.OLD_NETHER;
-
-        return dimension;
-    }
 }
