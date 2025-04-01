6cbabcb2729d642bf8f2ca1fb70864939e49ee81
Naz <bluspringindohq@gmail.com>
2025-04-01 18:50:04 +0800

    Try fix old Nether teleport

--- src/main/java/xyz/bluspring/valleyutils/mixin/tweaks/old_dimension/EntityMixin.java
+++ src/main/java/xyz/bluspring/valleyutils/mixin/tweaks/old_dimension/EntityMixin.java
@@ -1,5 +1,6 @@
 package xyz.bluspring.valleyutils.mixin.tweaks.old_dimension;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
@@ -12,6 +13,30 @@ import xyz.bluspring.valleyutils.ValleyUtils;
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Shadow public abstract Level level();
+
+    @ModifyExpressionValue(method = "findDimensionEntryPoint", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;dimension()Lnet/minecraft/resources/ResourceKey;"))
+    private ResourceKey<Level> addOldDimensionHandling(ResourceKey<Level> original) {
+        if (this.level().dimension() == ValleyUtils.OLD_END)
+            return Level.END;
+        else if (this.level().dimension() == ValleyUtils.OLD_NETHER)
+            return Level.NETHER;
+        else if (this.level().dimension() == ValleyUtils.OLD_OVERWORLD)
+            return Level.OVERWORLD;
+
+        return original;
+    }
+
+    @ModifyExpressionValue(method = "findDimensionEntryPoint", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;dimension()Lnet/minecraft/resources/ResourceKey;"))
+    private ResourceKey<Level> addOldDimensionHandling2(ResourceKey<Level> original) {
+        if (this.level().dimension() == ValleyUtils.OLD_END)
+            return Level.END;
+        else if (this.level().dimension() == ValleyUtils.OLD_NETHER)
+            return Level.NETHER;
+        else if (this.level().dimension() == ValleyUtils.OLD_OVERWORLD)
+            return Level.OVERWORLD;
+
+        return original;
+    }
 
     @ModifyArg(method = "handleNetherPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getLevel(Lnet/minecraft/resources/ResourceKey;)Lnet/minecraft/server/level/ServerLevel;"))
     private ResourceKey<Level> useOldDimension(ResourceKey<Level> dimension) {
